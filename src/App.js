c27a1c2afb56877de22ebf23f677a3415269db23
Mukherjee <Molly.Mukherjee@mind.motherson.com>
2023-06-13 14:03:30 +0530

    addedfrontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Signup from './components/authentication/Signup';
+import Login from './components/authentication/Login';
+import AdminLogin from './components/adminAuth/index';
+import Forgot from './components/authentication/Forgot';
+import UpdateBook from './components/UpdateBook/index';
+import AddBooks from './components/AddBooks/index';
+
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import BookDashboard from './components/BookDashboard/index';
+import UserDashboard from './components/UserDashboard';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+      <Routes>
+        {/* <Route exact path="/" element={<MainPage />} /> */}
+
+        <Route exact path="/" element={<Login/>} />
+        {/* <Route exact path="/adminlogin" element={<AdminLogin/>} /> */}
+        <Route exact path="/signup" element={<Signup/>} />
+        <Route exact path="/booksData" element={<BookDashboard/>} />
+        <Route path='/addData' element={<AddBooks/>}/>
+        <Route exact path="/userData" element={<UserDashboard/>} />
+		    <Route path='/update/:id' element={<UpdateBook/>}/>
+		    <Route path='/forgot' element={<Forgot/>}/>
+        </Routes>
+    </Router>
+
     </div>
   );
 }
-
 export default App;
